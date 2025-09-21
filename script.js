2e6035ec7df8db55bee4e2ba046914914cdc7228
Linkm118 <206734997+Linkm118@users.noreply.github.com>
2025-09-21 11:19:22 +0900

    Update script.js

--- script.js
+++ script.js
@@ -78,7 +78,7 @@ const Qimg = [
 ]; //問題画像
 const Corr = [
   'さいころ',
-  'すいか',
+  'チーター',
   '心音',
 
   '東京',
@@ -210,6 +210,7 @@ function nextquestion(Q) {
 //   correctans = kiCorr[kQuizNum];
 //   console.log(kQuizNum);
 // }
+
 
 
 
